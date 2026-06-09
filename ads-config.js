635c4e5179c309941725c71a5e225f56b8dd0c01
liam1234569 <liamgartner1234@gmail.com>
2026-06-09 15:31:32 +0200

    Update ads-config.js

--- ads-config.js
+++ ads-config.js
@@ -7,4 +7,9 @@ const adsConfig = [
         image: "5d711db0-95e3-4c9a-93e9-d8c911a238af.png", 
         link: "https://liam1234569.github.io/christ-life/"
     },
+    {
+        name: "Tech OS",
+        image: "4522ff2b-e7de-4e3c-8bb9-d84b622b46bb.png",
+        link: "https://liam1234569.github.io/Tech/"
+    },
 ]
