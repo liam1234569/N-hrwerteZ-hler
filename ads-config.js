2180b95734b3d79900c91b88544a3f52d6bc1b5d
liam1234569 <liamgartner1234@gmail.com>
2026-06-09 14:50:17 +0200

    Add files via upload

---
+++ ads-config.js
@@ -0,0 +1,10 @@
+// HIER KANNST DU DEINE WERBUNG BEARBEITEN
+// Einfach Bild-URL, Name und Link austauschen oder neue Blöcke hinzufügen!
+
+const adsConfig = [
+    {
+        name: "Christ Life",
+        image: "5d711db0-95e3-4c9a-93e9-d8c911a238af.png", 
+        link: "https://liam1234569.github.io/christ-life/"
+    },
+]
